8568eeacaa05c522212269ed3f7e2fb26fc70526
unknown <df.serrano@uniandes.edu.co>
2020-03-14 17:44:37 -0500

    buffer corregido

--- bufferTCP.py
+++ bufferTCP.py
@@ -40,38 +40,3 @@ class Buffer:
         if '\x00' in s:
             raise ValueError('string contains delimiter(null)')
         self.sock.sendall(s.encode() + b'\x00')
-With this class, your client and server become:
-
-client.py:
-
-import socket
-import threading
-import os
-
-import buffer
-
-HOST = '127.0.0.1'
-PORT = 2345
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-
-with s:
-    sbuf = buffer.Buffer(s)
-
-    hash_type = input('Enter hash type: ')
-
-    files = input('Enter file(s) to send: ')
-    files_to_send = files.split()
-
-    for file_name in files_to_send:
-        print(file_name)
-        sbuf.put_utf8(hash_type)
-        sbuf.put_utf8(file_name)
-
-        file_size = os.path.getsize(file_name)
-        sbuf.put_utf8(str(file_size))
-
-        with open(file_name, 'rb') as f:
-            sbuf.put_bytes(f.read())
-        print('File Sent')
