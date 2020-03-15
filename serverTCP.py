f7d95c6d528b827000e2e13cdf8ad908e67fc786
unknown <df.serrano@uniandes.edu.co>
2020-03-15 12:53:43 -0500

    from s to server

--- serverTCP.py
+++ serverTCP.py
@@ -91,18 +91,18 @@ while True:
             filename = files
             size = len(filename)
             size = bin(size)[2:].zfill(16) # encode filename size as 16 bit binary
-            s.send(size.encode())
-            s.send(filename.encode())
+            server.send(size.encode())
+            server.send(filename.encode())
 
             filename = os.path.join(path,filename)
             filesize = os.path.getsize(filename)
             filesize = bin(filesize)[2:].zfill(32) # encode filesize as 32 bit binary
-            s.send(filesize.encode())
+            server.send(filesize.encode())
 
             file_to_send = open(filename, 'rb')
 
             l = file_to_send.read()
-            s.sendall(l)
+            server.sendall(l)
             file_to_send.close()
             print( 'File Sent')
 
